5d2f21212f182f827508e66e880f3094b6ad9269
Takeshi Morita <takesy.morito@gmail.com>
2016-09-05 11:41:06 +0900

    add -t(wait millisecond)

--- logmon.go
+++ logmon.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"sync"
+	"fmt"
+	"time"
 	"flag"
 	"strings"
 	"io/ioutil"
@@ -18,6 +20,7 @@ type Watching struct {
 	Path string
 	Target *regexp.Regexp
 	Ignore *regexp.Regexp
+	WaitMillisecond int64
 	Command string
 }
 
@@ -36,9 +39,11 @@ func parseConf(contentStr string) []Watching {
 	fileRe :=  regexp.MustCompile("^:(.*)")
 	targetRe := regexp.MustCompile("^\\((.*)\\)$")
 	ignoreRe := regexp.MustCompile("^\\[(.*)\\]$")
+	timeRe := regexp.MustCompile("^{(.*)}$")
 	commandRe := regexp.MustCompile("^[^#].*")
 	var path string
 	var target, ignore *regexp.Regexp
+	var waitMillisecond int64
 	for i:=0; i < len(contents); i++ {
 		if fileRe.MatchString(contents[i]) {
 			if target != nil || ignore != nil {
@@ -47,20 +52,31 @@ func parseConf(contentStr string) []Watching {
 			path = fileRe.ReplaceAllString(contents[i], "$1")
 		} else if targetRe.MatchString(contents[i]) {
 			if path == "" {
-				panic(strconv.Itoa(i) + "target format error")
+				panic(strconv.Itoa(i) + " target appear before path")
 			}
 			target = regexp.MustCompile(targetRe.ReplaceAllString(contents[i], "$1"))
 		} else if ignoreRe.MatchString(contents[i]) {
 			if path == "" {
-				panic(strconv.Itoa(i) + "ignore format error")
+				panic(strconv.Itoa(i) + "ignore appear before path ")
 			}
 			ignore = regexp.MustCompile(ignoreRe.ReplaceAllString(contents[i], "$1"))
+		} else if timeRe.MatchString(contents[i]) {
+			if path == "" {
+				panic(strconv.Itoa(i) + "time appear before path")
+			}
+			waitMillisecondStr := timeRe.ReplaceAllString(contents[i], "$1")
+			milliSec, err := strconv.ParseInt(waitMillisecondStr, 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			waitMillisecond = milliSec
 		} else if commandRe.MatchString(contents[i]) {
 			if path == "" || target == nil {
 				panic(strconv.Itoa(i) + "command format error")
 			}
-			ret = append(ret, Watching{path, target,ignore, contents[i]})
+			ret = append(ret, Watching{path, target,ignore, waitMillisecond, contents[i]})
 			path = ""
+			waitMillisecond = 0
 			target = nil
 			ignore = nil
 		}
@@ -68,21 +84,50 @@ func parseConf(contentStr string) []Watching {
 	return ret
 }
 
-func logMonitor(data Watching) {
-	c := tail.Watch(data.Path)
+func escapeShell(s string) string {
+	message := strings.Replace(s, "'", "\\047", -1)
+	message = strings.Replace(message, "$", "\\044", -1)
+	return message
+}
+
+func executeCommand(conf Watching, targetMessage string){
 	replaceRe :=  regexp.MustCompile("<%%%%>")
+	command := replaceRe.ReplaceAllString(conf.Command, targetMessage)
+	_, err := exec.Command("sh", "-c", command).Output()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+}
+
+func logMonitor(conf Watching) {
+	c := tail.Watch(conf.Path)
+	var targetMessage string
+	var mutex sync.Mutex
 	for {
 		select {
 		case s := <-c:
-			if data.Target.MatchString(s) && (data.Ignore == nil || !data.Ignore.MatchString(s)) {
-				message := strings.Replace(s, "'", "\\047", -1)
-				message = strings.Replace(message, "$", "\\044", -1)
-				command := replaceRe.ReplaceAllString(data.Command, message)
-				out, err := exec.Command("sh", "-c", command).Output()
-				if err != nil {
-					panic(err)
+			if targetMessage != "" {
+				mutex.Lock()
+				if(targetMessage != ""){
+					targetMessage += ("\n" + escapeShell(s))
 				}
-				fmt.Println(string(out))
+				mutex.Unlock()
+			} else if conf.Target.MatchString(s) && (conf.Ignore == nil || !conf.Ignore.MatchString(s)) {
+				targetMessage += escapeShell(s)
+				if(conf.WaitMillisecond == 0){
+					executeCommand(conf, targetMessage)
+					targetMessage = ""
+				} else {
+					timer := time.NewTimer(time.Duration(conf.WaitMillisecond) * time.Millisecond)
+					go func() {
+						<-timer.C
+						mutex.Lock()
+						executeCommand(conf, targetMessage)
+						targetMessage = ""
+						mutex.Unlock()
+					}()
+				}
 			}
 		}
 	}
@@ -91,9 +136,9 @@ func logMonitor(data Watching) {
 func main(){
 	conf := flag.String("f", "/etc/logmon/logmon.conf", "config file(Default: /etc/logmon/logmon.conf)")
 	flag.Parse()
-	data := parseConf(readConf(*conf))
-	for i := range data {
-		go logMonitor(data[i])
+	confs := parseConf(readConf(*conf))
+	for i := range confs {
+		go logMonitor(confs[i])
 	}
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan,
